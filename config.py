cc3fdcc3099ae382dbbc4286483eada211765f33
Aj4ybot <118820596+Aj4ybot@users.noreply.github.com>
2024-02-23 19:16:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6733974538:AAE6TEyk0tsRy8jrwQx-u78ifzjXQw5wv3I")
-    API_ID =int(os.environ["API_ID", 21630147
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7128630149:AAH6ssqxm4YwUr26wlk0qtVPIYJDcgykah8")
+    API_ID =int(os.environ["API_ID", 22013352
     ]
-    API_HASH = os.environ["API_HASH", "394962449c1583fb83d4dbe1990acb3a"]
-    AUTH_USERS = "6082712650"
-    AUTH_GROUP = "-1002005516599"""
+    API_HASH = os.environ["API_HASH", "5cac3d97b3f3ceb4f59490253bd7f64c"]
+    AUTH_USERS = "5479730793"
+    AUTH_GROUP = "-1002043349024"""
