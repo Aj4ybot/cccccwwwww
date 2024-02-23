9d6833d3ed694d55c7b62e338dfe6de53bb0e894
Aj4ybot <118820596+Aj4ybot@users.noreply.github.com>
2024-02-23 19:13:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6733974538:AAE6TEyk0tsRy8jrwQx-u78ifzjXQw5wv3I")
+    API_ID =int(os.environ["API_ID", 21630147
+    ]
+    API_HASH = os.environ["API_HASH", "394962449c1583fb83d4dbe1990acb3a"]
+    AUTH_USERS = "6082712650"
+    AUTH_GROUP = "-1002005516599"""
